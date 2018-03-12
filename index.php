ca4179a2a19c0f8f2d091765d016a5dad1e0e7a5
Mathis Delaunay <delaunaymathis@yahoo.fr>
2018-03-12 16:22:32 +0100

    zd

--- index.php
+++ index.php
@@ -24,9 +24,11 @@
       <div id="meteo"></div>
       <div id="ping"></div>
       <!-- <div id="coindesk-widget"></div> -->
+      <div class="deezer">
         <div class="deezer-widget-player" data-src="https://www.deezer.com/plugins/player?format=classic&autoplay=true&playlist=true&width=200&height=200&color=007FEB&layout=dark&size=medium&type=playlist&id=1109890291&app_id=1" data-scrolling="no" data-frameborder="0" data-allowTransparency="true" data-width="200" data-height="200"></div>
+      </div>
     </div>
-<!-- 
+
     <script>
       (function(d, s, id) { 
       var js, djs = d.getElementsByTagName(s)[0];
@@ -35,7 +37,7 @@
       js.src = "https://e-cdns-files.dzcdn.net/js/widget/loader.js"; 
        djs.parentNode.insertBefore(js, djs);
     }(document, "script", "deezer-widget-loader"));
-    </script> -->
+    </script>
 
    <!--  <script>
       (function(b,i,t,C,O,I,N) {
